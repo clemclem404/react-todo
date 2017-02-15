a197b8fc7b5e9b4e8174f16396973c57d4d2553f
paulinerousseau <pauline_rousseau@hotmail.co.uk>
2017-02-15 16:25:35 -0500

    move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,10 +2,10 @@ import firebase from 'firebase';
 
 try {
   var config = {
-      apiKey: "AIzaSyD_7umDIERkcki_5Wix7V7aJ9avgNP3BvM",
-      authDomain: "pau-todo-app.firebaseapp.com",
-      databaseURL: "https://pau-todo-app.firebaseio.com",
-      storageBucket: "pau-todo-app.appspot.com",
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      storageBucket: process.env.STORAGE_BUCKET,
     };
 
     firebase.initializeApp(config);
